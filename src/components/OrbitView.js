00c76797969fe02135e3e353f0e45e2d61e3cd98
Lari Salminen <lari.j.a.salminen@student.jyu.fi>
2020-10-31 19:51:00 +0200

    Kiertoradan visualisaation front end ja ulkoasukokeiluja

---
+++ src/components/OrbitView.js
@@ -0,0 +1,51 @@
+"use strict";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+//var ecc = 0.1;
+//var sminax = Math.sqrt(1 - ecc * ecc);
+const ballRadius = 5;
+
+const OrbitView = ({ eccentricity }) => {
+	let ecc = 0.5;
+	ecc = eccentricity;
+	let sminax = Math.sqrt(1 - ecc * ecc);
+
+	return (
+		<View
+			style={{
+				flex: 2,
+				backgroundColor: "black",
+				justifyContent: "center",
+				alignItems: "center",
+			}}>
+			<View style={styles.orbit(sminax)}>
+				<View style={styles.ball(ecc, sminax)}></View>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	orbit: (sminax) => ({
+		width: 200,
+		flexDirection: "row",
+		height: 200,
+		borderRadius: 100,
+		borderWidth: 1,
+		borderColor: "white",
+		transform: [{ scaleY: sminax }],
+		justifyContent: "center",
+		alignItems: "center",
+	}),
+	ball: (eccentricity, sminax) => ({
+		width: ballRadius,
+		height: ballRadius,
+		left: -100 * eccentricity,
+		borderRadius: ballRadius / 2,
+		backgroundColor: "yellow",
+		transform: [{ scaleY: 1 / sminax }],
+	}),
+});
+
+export default OrbitView;
--- src/screens/Distance.js
+++ src/screens/Distance.js
@@ -1,53 +1,98 @@
-import React from 'react'
+import React from "react";
 import { Text, View, Dimensions, StyleSheet } from "react-native";
-import PlanetDistanceComparison from '../components/PlanetDistanceComparison';
+import PlanetDistanceComparison from "../components/PlanetDistanceComparison";
+import OrbitView from "../components/OrbitView";
 
 const Distance = ({ route }) => {
-  console.log('const dist', route.params.planet)
-  const planet = route.params.planet
-  return (
-    <View style={styles.container}>
-      <View style={styles.distanceBox}>
-        <PlanetDistanceComparison planet={planet}/>
-      </View>
-      <View style={styles.infoBox}>
-        <Text style={styles.infoBoxTitle}>{planet.pname}</Text>
-          <Text style={styles.infoBoxContent}>
-            {planet.pname} has a
-            <Text style={{color: 'blue' }} onPress={() => {alert('Tässä selitys');}}> semimajor axis </Text> 
-            of {planet.pl_orbsmax} AU.
-          </Text>
-      </View>
-    </View>
-  )
-}
+	console.log("const dist", route.params.planet);
+	const planet = route.params.planet;
+	return (
+		<View style={styles.container}>
+			<View style={styles.distanceBox}>
+				<PlanetDistanceComparison planet={planet} />
+			</View>
+			<View style={styles.infoBox}>
+				<View style={styles.infoBox}>
+					<Text style={styles.infoBoxTitle}>{planet.pname}</Text>
+					<Text style={styles.infoBoxContent}>
+						{planet.pname} has a
+						<Text
+							style={{ color: "lightblue" }}
+							onPress={() => {
+								alert(
+									"The Semimajor axis is the distance from the center of an ellipse to its farthest edge. It can also be thought of as a rough estimate of the average distance of the planet from its host star."
+								);
+							}}>
+							{" "}
+							semimajor axis{" "}
+						</Text>
+						of{" "}
+						<Text style={{ color: "yellow" }}>{planet.pl_orbsmax + " "}</Text>
+						<Text
+							style={{ color: "lightblue" }}
+							onPress={() => {
+								alert(
+									"An astronomical unit (AU) is 149,597,900km, or roughly the average distance of the Earth from the sun."
+								);
+							}}>
+							AU
+						</Text>
+						.
+					</Text>
+					<Text style={styles.infoBoxContent}>
+						It completes a full orbit around its host star every{" "}
+						<Text style={{ color: "yellow" }}>TODO</Text> days, or once every
+						<Text style={{ color: "yellow" }}> TODO/365</Text> Earth years.
+					</Text>
+					<Text style={styles.infoBoxContent}>
+						{planet.pname}'s orbit has an{" "}
+						<Text
+							style={{ color: "lightblue" }}
+							onPress={() => {
+								alert(
+									"Eccentricity is a measure of how elliptical the orbit is, with 0 being perfectly circular and getting more elliptical as it approaches 1."
+								);
+							}}>
+							eccentricity
+						</Text>{" "}
+						of <Text style={{ color: "yellow" }}>TODO</Text>, making it
+						more/less elliptical than Earth's orbit.
+					</Text>
+				</View>
+				<OrbitView eccentricity={0.3}></OrbitView>
+			</View>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    flexDirection: 'row',
-    backgroundColor: 'black',
-    justifyContent: 'center'
-  },
-  distanceBox: {
-    flex:2,
-    //width: Dimensions.get('window').width /2,
-  },
-  infoBox: {
-    flex:3,
-    //width: Dimensions.get('window').width /2,
-    backgroundColor: 'white'
-  },
-  infoBoxTitle: {
-    fontSize: 30,
-    margin: 10
-  },
-  infoBoxContent: {
-    marginLeft: 10,
-    marginRight: 20,
-    textAlign: 'justify'
+	container: {
+		flex: 1,
+		flexDirection: "row",
+		backgroundColor: "black",
+		justifyContent: "center",
+	},
+	distanceBox: {
+		flex: 2,
+		//width: Dimensions.get('window').width /2,
+	},
+	infoBox: {
+		flex: 3,
+		//width: Dimensions.get('window').width /2,
+		backgroundColor: "black",
+	},
+	infoBoxTitle: {
+		fontSize: 30,
+		margin: 10,
+		color: "white",
+	},
+	infoBoxContent: {
+		marginLeft: 10,
+		marginRight: 20,
+		marginBottom: 8,
+		color: "white",
+		fontSize: 20,
+	},
+});
 
-  }
-})
-
-export default Distance
+export default Distance;
